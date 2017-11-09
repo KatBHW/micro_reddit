9dd3baeef7ad5bd54cd634642fd490ee2ba2a07c
Kathryn Williams <ksw4321@gmail.com>
2017-11-09 17:22:56 +0000

    basic models and validations completed

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  validates :title,  presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 500 }
+  validates :user_id, presence: true
+end
+
+
